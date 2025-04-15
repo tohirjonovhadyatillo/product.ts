be218fe3e69db4253407fb804f5a0371c14e910b
tohirjonovhadyatilo <mobiletillopubg@gmail.com>
2025-04-15 14:26:29 +0500

    refactor: remove unnecessary React import and update TypeScript configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
 import Products from './pages/Products';
