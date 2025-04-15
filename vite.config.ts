46feef77d0ed57a8002784001e40b9f8aa3d073d
tohirjonovhadyatilo <mobiletillopubg@gmail.com>
2025-04-15 14:34:11 +0500

    style: format code for consistency in main.tsx and vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(),tailwindcss(),],
 })
